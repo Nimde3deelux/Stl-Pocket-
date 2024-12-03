5848f583addc8faa3451fc34b2d9dec5d0cbefa6
sshhaall2601 <robin.badigami@gmail.com>
2024-12-03 16:37:06 +0100

    Update exercices.js

--- exercices.js
+++ exercices.js
@@ -1,4 +1,13 @@
-// Fonction pour gérer le clic sur un des rectangles
-function handleClick(subject, type) {
-    console.log(`Clique sur ${type} de la matière ${subject}`);
+function navigateTo(page) {
+    window.location.href = page;
+}
+
+function highlight(id) {
+    const section = document.getElementById(id);
+    section.style.transform = "scale(1.3) translateY(-10px)";
+}
+
+function reset(id) {
+    const section = document.getElementById(id);
+    section.style.transform = "scale(1) translateY(0)";
 }
